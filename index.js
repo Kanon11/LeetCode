099f00fdb7319b817d6c930f3f6c858426f96489
Kanon11 <kanonshet@yahoo.com>
2022-09-10 23:33:14 +0600

    kc

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-var majorityElement = function (nums) {
-    var n = nums.length;
-    var m = parseInt(n / 2);
-    var c = 0;
-    let item = null;
+
+// let ans = findDuplicate([1, 2, 2, 3]);
+// console.log(ans);
+var findErrorNums = function (nums) {
+    let n = nums.length;
+    let duplicate;
+
+    let sum = 0;
+    let s = new Set();
     for (let index = 0; index < n; index++) {
-        c = 0;
-        item = nums[index];
-        for (let j = 0; j < n; j++){
-            if (item == nums[j]) {
-                c++;
-                if (c > m) {
-                    return item;
-                }
-            }
+        if (s.has(nums[index])) {
+            duplicate = nums[index];
+            continue;
         }
-        
+        s.add(nums[index]);
     }
+    let myiterator = s.values();
+    for (const iterator of myiterator) {
+        sum += iterator;
+    }
+    let cal = parseInt((n * (n + 1) / 2));
+    return [duplicate,cal-sum];
+    
 
 };
+// let ans = findErrorNums([23, 21, 24, 27, 22, 27, 26, 25]);
+let ans = findErrorNums([1, 2, 3, 4, 4, 5]);
+// let ans = findErrorNums([1, 2,2,4]);
+console.log(window);
 
-var nums = [2, 2, 1, 1, 1, 2, 2];
-console.log(majorityElement(nums));
