ca5a7cdb93ea3140f16d53141ea7d9c3a414fbf0
Kanon11 <kanonshet@yahoo.com>
2022-09-16 15:43:52 +0600

    curl CURLOPT_TIMEOUT

---
+++ curl.php
@@ -0,0 +1,27 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://127.0.0.1:80/LeetCode/data.php',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 0,
+    CURLOPT_TIMEOUT => 4,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+if (curl_errno($curl)) {
+    $error_msg = curl_error($curl);
+}
+curl_close($curl);
+
+if (isset($error_msg)) {
+    echo $error_msg;
+    die();
+}
+
+echo $response;
