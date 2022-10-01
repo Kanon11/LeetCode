0d22b137506c5790d6c8b7b61047f0b847068362
Kanon11 <kanonshet@yahoo.com>
2022-10-01 23:47:16 +0600

    kc

--- group-anagram.js
+++ group-anagram.js
@@ -1,68 +1,30 @@
-var isAnagram = function (s, t) {
-    if (s.length < t.length) {
-        return false;
-    }
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+var topKFrequent = function (nums, k) {
 
-    let s_map = new Map();
-    let t_map = new Map();
-
-    for (let i = 0; i < s.length; i++) {
-        if (s_map.has(s[i])) {
-            s_map.set(s[i], s_map.get(s[i]) + 1);
+    let map = new Map;
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+        if (map[element] != null) {
+            map[element] = (map[element] + 1);
         }
         else {
-            s_map.set(s[i], 1)
+            map[element] = 1;
         }
 
     }
-    for (let j = 0; j < t.length; j++) {
-        if (t_map.has(t[j])) {
-            t_map.set(t[j], t_map.get(t[j]) + 1);
-        }
-        else {
-            t_map.set(t[j], 1);
-        }
-    }
-    for (let x of s_map.keys()) {
-        if (t_map.has(x)) {
-            if (s_map.get(x) === t_map.get(x)) {
-                continue;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return false;
-        }
-    }
+    console.log(map);
+    let ans = new Map();
+    for (let j = 1; j <= nums.length; j++) {
+        let x = getKeyByValue(map, j);
+        console.log('x', x);
 
-    return true;
+    };
+    console.log(ans);
+
 };
-
-var groupAnagrams = function (strs) {
-
-    let g_anagram = new Map();
-    for (let index = 0; index < strs.length; index++) {
-        const element = strs[index];
-        if (index === 0) {
-            g_anagram[0][0] = element;
-        }
-        else {
-            for (let i = 0; i < g_anagram.length; i++) {
-                const g = g_anagram[i][0];
-                if (isAnagram(element,g)) {
-                    g_anagram[i].push(g);
-                }      
-            }
-            g_anagram[g_anagram.length].push(element);
-        }
-
-        
-    }
-    return g_anagram;
-};
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
-// strs = [""];
-let res = groupAnagrams(strs);
-console.log(res);
+var nums = [1, 1, 1, 2, 2,2, 3, 3];
+var k = 2;
+let ff = topKFrequent(nums, k);
+// console.log(ff);
