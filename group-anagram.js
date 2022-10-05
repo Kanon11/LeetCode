580feb7f5e910aeef0bdf06552ff0da8faa75029
Kanon11 <kanonshet@yahoo.com>
2022-10-05 17:54:36 +0600

    home

--- group-anagram.js
+++ group-anagram.js
@@ -1,12 +1,17 @@
 function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
 }
+function getkeys(map,value) {
+    map.forEach(element => {
+        console.log(element);
+    });
+}
 var topKFrequent = function (nums, k) {
 
     let map = new Map;
     for (let i = 0; i < nums.length; i++) {
-        const element = nums[i];
-        if (map[element] != null) {
+        const element =  nums[i];
+        if (map[ element] != null) {
             map[element] = (map[element] + 1);
         }
         else {
@@ -15,13 +20,12 @@ var topKFrequent = function (nums, k) {
 
     }
     console.log(map);
-    let ans = new Map();
-    for (let j = 1; j <= nums.length; j++) {
-        let x = getKeyByValue(map, j);
-        console.log('x', x);
-
-    };
-    console.log(ans);
+    for (let [currentNumber, frequency] of map) { 
+        console.log([currentNumber, frequency]);
+    }
+    // for (let j = 0; j < nums.length; j++) {
+    //     console.log(map[1]);
+    // };
 
 };
 var nums = [1, 1, 1, 2, 2,2, 3, 3];
