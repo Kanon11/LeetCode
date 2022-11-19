e5b82ad50aa7ba236d5b10c6f998b190ffa7e9f9
Kanon11 <kanonshet@yahoo.com>
2022-11-19 09:24:44 +0600

    add

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-/**
- * @param {number[]} prices
- * @return {number}
- */
-var maxProfit = function (prices) {
-    let profit = [];
-    for (let i = 0; i < prices.length; i++) {
-        for (let j = i; j < prices.length; j++) {
-            if (profit[j] == undefined) {
-                profit[j] = prices[j ] - prices[i];
-            }
-            else {
-                profit[j] = Math.max(profit[j], prices[j] - prices[i]);
-            }
-            
-        }
-        
-    }
-    profit = profit.sort((a, b) => {return a - b });
-    return (profit[profit.length-1]);
-};
-
-let prices = [7, 1, 5, 3, 6, 4];
-=======
 var maxProfit = function (prices) {
     let max_tk = 0;
     let l = 0;
@@ -41,5 +16,4 @@ var maxProfit = function (prices) {
 };
 
 let prices = [2, 1, 2, 1, 0, 1, 2];
->>>>>>> 542da1b666cebc62f22830768315937aa113e83d
 console.log(maxProfit(prices));
