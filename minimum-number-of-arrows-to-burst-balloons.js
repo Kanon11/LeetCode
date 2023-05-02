52d644bf603b045bf83f2a465ac6f76e75d85ff4
kanonmomagic <kanon@momagicbd.com>
2023-05-02 13:05:16 +0600

    kc

--- minimum-number-of-arrows-to-burst-balloons.js
+++ minimum-number-of-arrows-to-burst-balloons.js
@@ -10,40 +10,22 @@ var findMinArrowShots = function (points) {
         }
     }
     console.log(points);
-    let array = [];
-    let count = 0;
-    for (let i = 0; i < points.length - 1;) {
-        if (points[i][1] < points[i + 1][0]) {
-            array.push(points[i]);
-            array.push(points[i + 1]);
-            i++;
-            count += 2;
-            console.log('1')
-        }
-        else if (points[i][0] <= points[i + 1][0] && points[i][1] <= points[i + 1][1]) {
-                array.push([points[i][0], points[i + 1][1]]);
-                i++;
-                count++;
-                console.log('2')
-        }
-        else if (points[i][0] < points[i+1][0] && points[i][1] > points[i+1][1]) {
-            array.push([points[i][0], points[i][1]]);
-            i++
-            count++;
-            console.log('3')
 
+    let arrow_count = 1;
+    let end_point = points[0][1];
+    for (let i = 1; i < points.length;i++) {
+        if (end_point>=points[i][0]) {
+            end_point = Math.min(end_point, points[i][1]);
+        } else {
+            arrow_count++;
+            end_point = points[i][1];
+            
         }
-        else {
-            console.log('else')
-        }
-        // else if(points[i][1]<=points[i+1][0])
-
     }
-    console.log(array)
-    return count?count:1;
+    return arrow_count;
 
 };
-// console.log(findMinArrowShots([[1, 2], [4, 5], [1, 5]]))
-console.log(findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]]))
+console.log(findMinArrowShots([[1, 2], [4, 5], [1, 5]]))
+// console.log(findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]]))
 // console.log(findMinArrowShots([[1, 2], [3, 4], [5, 6], [7, 8]]))
 // console.log(findMinArrowShots([[1, 2], [2, 3], [3, 4], [4, 5]]))
---
+++ tree.js
@@ -0,0 +1,21 @@
+class Node {
+
+    constructor(data) {
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+}
+class BSTree{
+    constructor() {
+        this.root = null;
+    }
+    insert(data) {
+        
+    }
+
+}
+let a = [];
+let b = [1, 3];
+a.push(b);
+console.log(a)
