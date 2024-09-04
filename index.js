b23271446da01b874125e25deaee0ee37d3cb718
Kanon11 <kanoncuetcse11@gmail.com>
2024-09-04 08:38:27 +0600

    k

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-
-// let ans = findDuplicate([1, 2, 2, 3]);
-// console.log(ans);
-var findErrorNums = function (nums) {
-    let n = nums.length;
-    let duplicate;
-
-    let sum = 0;
-    let s = new Set();
-    for (let index = 0; index < n; index++) {
-        if (s.has(nums[index])) {
-            duplicate = nums[index];
-            continue;
-        }
-        s.add(nums[index]);
-    }
-    let myiterator = s.values();
-    for (const iterator of myiterator) {
-        sum += iterator;
-    }
-    let cal = parseInt((n * (n + 1) / 2));
-    return [duplicate,cal-sum];
-    
-
-};
-// let ans = findErrorNums([23, 21, 24, 27, 22, 27, 26, 25]);
-let ans = findErrorNums([1, 2, 3, 4, 4, 5]);
-// let ans = findErrorNums([1, 2,2,4]);
-console.log(window);
-
+// import  {log100}  from "./script.js"
+// const {log100,sum}=require("./script.js")
+import {sum,log100} from "./script.js"
+console.log(sum(1,100000))
---
+++ script.js
@@ -0,0 +1,11 @@
+let log100 = (x) => {
+    return Math.log(x) / Math.log(100);
+}
+let sum = (x, y) => {
+    return x + y;
+}
+// module.exports = { log100,sum };
+
+// export default sum;
+export { sum, log100 };
+// export {sum,log100}
